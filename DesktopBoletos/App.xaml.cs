459b0907b420a7cde984a6255d0cae592ed0c330
Anderson <Andersongomez9870@gmail.com>
2023-06-09 14:40:22 -0400

    First commit Desktop aplication

---
+++ DesktopBoletos/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesktopBoletos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+	private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs args)
+        {
+            MessageBox.Show($"Ocurrio un error:( {args.Exception.Message}", "Error no controlado", MessageBoxButton.OK, MessageBoxImage.Error);
+            args.Handled = true;
+        }
+    }
+}
