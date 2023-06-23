97a8bcb6f95db3951d9f88147e69e19a76d8dfa2
Anderson <Andersongomez9870@gmail.com>
2023-06-23 14:23:20 -0400

    Creando modulo del app (falta la URL del api).

---
+++ AppBoletos/app/src/main/java/edu/ucne/appboletos/di/AppModule.kt
@@ -0,0 +1,36 @@
+package edu.ucne.appboletos.di
+
+import javax.inject.Singleton
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import edu.ucne.appboletos.data.remote.EventoApi
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiBoletas(moshi: Moshi): EventoApi {
+        val Base_Url = "http://"
+        return Retrofit.Builder()
+            .baseUrl(Base_Url)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(EventoApi::class.java)
+    }
+}
